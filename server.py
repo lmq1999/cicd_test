79b64f662eb45809f28f977707faa30cb86a4654
Lê Minh Quân <quanlm1999@gmail.com>
2023-05-09 16:39:57 +0700

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Xin ch%s' % (self.path)
+        msg = 'Xin chao %s' % (self.path)
         self.wfile.write(msg.encode())
 
 port = int(os.getenv('PORT', 80))
